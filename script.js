0cedaa44e49e53c6635a385dfbb5fa381050c22c
Jefferson Adams <jeff.adams@hotmail.com>
2017-07-06 20:05:39 -0400

    second commit, added quantity

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
 var total = 0;
-var taxTotal = 0;
-var taxRate = 1.06;
+var totalTax = 0;
+var taxedTotal = 0;
+var taxRate = 0.06;
 
 var inventory = [
   {
     name: "apple",
     price: 1.00,
-    tax: false
+    tax: false,
+    quantity: 5
   },
   {
     name: "t-shirt",
     price: 9.95,
-    tax: true
+    tax: true,
+    quantity: 1
   },
   {
     name: "watermelon",
     price: 3.49,
-    tax: false
+    tax: false,
+    quantity: 1
   },
   {
     name: "spray paint",
     price: 3.89,
-    tax: true
+    tax: true,
+    quantity: 2
   },
   {
     name: "strawberries",
     price: 4.19,
-    tax: false
+    tax: false,
+    quantity: 3
   },
 ];
 
-alskdjfalksjdhflskjdfa;
-
 inventory.forEach(function(x){
   //print out name and price on each line
-  console.log((x.name) + " - $" + (x.price).toFixed(2))
+  console.log((x.quantity) + " x " + (x.name) + " = $" + (x.price).toFixed(2))
 });
 
 for (var i=0; i < inventory.length; i++){
   if (inventory[i].tax === true){
-    total = ((inventory[i].price) * taxRate) + total;
+    total = ((inventory[i].price) * (1 + taxRate)) * (inventory[i].quantity) + total;
+    totalTax = ((inventory[i].price) * (inventory[i].quantity) *  taxRate) + totalTax;
   } else {
-    total = inventory[i].price + total;
+    total = inventory[i].price * inventory[i].quantity + total;
   }
 }
-console.log("total price - $" + total.toFixed(2));
+console.log("total tax = $" + totalTax.toFixed(2));
+console.log("total price = $" + total.toFixed(2));
